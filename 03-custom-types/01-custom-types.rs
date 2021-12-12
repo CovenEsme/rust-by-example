c7a762bf16d0489960db04c56f7234e76e387422
elliebeph <84377742+elliebeph@users.noreply.github.com>
2021-12-12 17:58:10 +0000

    Add file listing differect types of custom type

---
+++ 03-custom-types/01-custom-types.rs
@@ -0,0 +1,5 @@
+// Custom types are formed mainly through two keywords:
+// * `struct` defines a structure,
+// * `enum` defines an enumeration.
+//
+// Constants are created using the `const` and `static` keywords.
