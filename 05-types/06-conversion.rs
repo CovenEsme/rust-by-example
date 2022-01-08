cf76989aa02f622493de375c462dcede704a592a
elliebeph <84377742+elliebeph@users.noreply.github.com>
2022-01-08 09:49:19 +0000

    Add file with an overview of type conversion

---
+++ 05-types/06-conversion.rs
@@ -0,0 +1,5 @@
+// Primative types can be converted to each other through `casting`.
+
+// Conversion between custom types is achieved through the use of `traits`.
+// Generally, this is done with the `From` and `Into` traits. However, there
+// also more specific ones for common conversions - e.g. to and from `String`s.
